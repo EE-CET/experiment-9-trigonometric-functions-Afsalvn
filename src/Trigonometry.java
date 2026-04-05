7999a08ab688b1e0de990b0b1b706e1723a699c2
Afsalvn <147943587+Afsalvn@users.noreply.github.com>
2026-04-05 09:35:28 +0530

    Implement sine and cosine methods in Trigonometry

--- src/Trigonometry.java
+++ src/Trigonometry.java
@@ -1,14 +1,36 @@
+import java.util.Scanner;
+import java.lang.Math;
+
 public class Trigonometry {
 
-    // TODO: Create 'public static double sine(double angle)'
-    
+    // Method to calculate Sine from degrees
+    public static double sine(double angle) {
+        // Convert degrees to radians as required by Math.sin
+        double radians = Math.toRadians(angle);
+        return Math.sin(radians);
+    }
 
-    // TODO: Create 'public static double cosine(double angle)'
-    
+    // Method to calculate Cosine from degrees
+    public static double cosine(double angle) {
+        // Convert degrees to radians as required by Math.cos
+        double radians = Math.toRadians(angle);
+        return Math.cos(radians);
+    }
 
-    
-        // TODO: Read angle
-        // TODO: Call sine() and cosine()
-        // TODO: Print results in the required format
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        if (sc.hasNextDouble()) {
+            double angleInDegrees = sc.nextDouble();
+            
+            // Calling static methods directly using the class name
+            double sVal = TrigCalculator.sine(angleInDegrees);
+            double cVal = TrigCalculator.cosine(angleInDegrees);
+            
+            // Output formatting
+            System.out.println("Sine: " + sVal);
+            System.out.println("Cosine: " + cVal);
+        }
+        sc.close();
+    }
 }
